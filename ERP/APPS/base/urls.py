3f04b38b086864beae158be63d752c567a9e3c89
yorlysoro <yorlysoro@gmail.com>
2021-05-17 20:35:20 -0400

    Added bootstrap files css js bootstrap-icons

--- ERP/APPS/base/urls.py
+++ ERP/APPS/base/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-from django.views.generic import TemplateView
+from APPS.base.views import IndexView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name="base/index.html")),
+    path('', IndexView.as_view(), name='index'),
 ]
