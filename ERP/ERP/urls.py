f5b80c5c2f082bf59834afcce8385a32f861f941
yorlysoro <yorlysoro@gmail.com>
2023-04-28 21:20:06 -0400

    upgrade requirements disabled some incompatible packages

--- ERP/ERP/urls.py
+++ ERP/ERP/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     # path('admin/', include('admin_honeypot.urls', namespace='admin_honeypot')),
     path('secret/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
-    path('auth/', include('rest_auth.urls')),
-    path('auth/registration/', include('rest_auth.registration.urls')),
+    # path('auth/', include('rest_auth.urls')),
+    # path('auth/registration/', include('rest_auth.registration.urls')),
     # path('secret/defender/', include('defender.urls')), # defender admin
 ]
